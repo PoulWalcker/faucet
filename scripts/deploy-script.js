1b8ebbade236533193e304d187e05df0b0993b33
Alex Telegin <telegin.alexander@gmail.com>
2022-04-27 17:07:52 +0400

    Add verification, FTM deploy

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -7,6 +7,13 @@ async function main() {
   console.log("NFT deployed to: ", nft.address);
 }
 
+async function verify() {
+  await hre.run("verify:verify", {
+    address: "0x6d0E7094a385396F78399b5c679be09d8702555B",
+    network: "ftm",
+  });
+}
+
 main().then(() => process.exit(0)).catch(error => {
   console.error(error);
   process.exit(1);
--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("GTONMemorableNFT");
   const signer = await hre.ethers.getSigner();
-  const contractAddress = "";
+  const contractAddress = "0x6d0E7094a385396F78399b5c679be09d8702555B"; // Fantom mainnet
   const contract = NFT.attach(contractAddress);
   let nftId = await contract.mint(signer.address);
   console.log("NFT minted: ", nftId);
