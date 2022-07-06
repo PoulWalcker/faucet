1f21a5b2c1e029f28cef8d2f68fd512dd4cc9b3a
PoulWalcker <pashasimanko123@gmail.com>
2022-07-06 10:23:38 +0400

    Update contract for few tokens

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -14,29 +14,23 @@ contract Faucet is InitializableOwnable  {
      
     mapping(address => uint) public lastTimestampList;
     uint public interval = 86400;
-    IERC20 token;
 
-    constructor(address _tokenAddr) {
+    constructor() {
         initOwner(msg.sender);
-        token = IERC20(_tokenAddr);
     }
 
     // Sends the amount of token to the caller.
-    function send() external {
-        uint amount = 200 * (10 ** token.decimals());
+    function send(IERC20 _token) external {
+        uint amount = 200 * (10 ** _token.decimals());
         
-        require(token.balanceOf(address(this)) > amount, "FaucetError: Empty");
+        require(_token.balanceOf(address(this)) > amount, "FaucetError: Empty");
         require(block.timestamp - lastTimestampList[msg.sender] > interval, "FaucetError: Try again later");
     
         lastTimestampList[msg.sender] = block.timestamp;
         
-        require(token.transfer(msg.sender, amount));
+        require(_token.transfer(msg.sender, amount));
     }  
 
-    // Updates the underlying token address
-    function setTokenAddress(address _tokenAddr) external onlyOwner {
-        token = IERC20(_tokenAddr);
-    } 
 
      // Updates the interval
     function setFaucetInterval(uint256 _interval) external onlyOwner {
